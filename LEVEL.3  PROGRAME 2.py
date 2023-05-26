846c3b650de3e0feefa7f3fe6db9580a893b991e
neroghcedrick <134517782+NEROGH2802@users.noreply.github.com>
2023-05-26 21:42:23 +0530

    Add files via upload

--- LEVEL.3  PROGRAME 2.py
+++ LEVEL.3  PROGRAME 2.py
@@ -1,67 +1,19 @@
-import turtle as t 
+import turtle
 
-def rectangle(horizontal,vertical,color):
-    t.pendown()
-    t.pensize(1)
-    t.color(color)
-    t.begin_fill()
-    for counter in range(1,3):
-        t.forward(horizontal)
-        t.right(90)
-        t.forward(vertical)
-        t.right(90)
-    t.end_fill()
-    t.penup()
+from itertools import cycle
+colors = cycle(['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
 
-t.penup()
-t.speed('slow')
-t.bgcolor('Dodger blue')
 
-#feet
-t.goto(-100, -150)
-rectangle(50,20,'blue')
-t.goto(-30,-150)
-rectangle(50,20,'blue')
+def draw_circle(size,angle,shift):
+    turtle.pencolor(next(colors))
+    turtle.circle(size+20)
+    turtle.right(angle+20)
+    turtle.forward(shift+10)
+    draw_circle(size+20, angle+20,shift+10)
+    
+turtle.bgcolor('black')
+turtle.speed('fast')
+turtle.pensize(4)
+draw_circle(30,0,1)
 
-#legs
-t.goto(-25, -50)
-rectangle(15,100,'grey')
-t.goto(-55,-50)
-rectangle(-15,100,'grey')
 
-#body
-t.goto(-90,100)
-rectangle(100,150,'red')
-
-#arms
-t.goto(-150, 70)
-rectangle(60,15,'grey')
-t.goto(-150,110)
-rectangle(15,40,'grey')
-
-t.goto(10, 70)
-rectangle(60,15,'grey')
-t.goto(55,110)
-rectangle(15,40,'grey')
-
-#neck
-t.goto(-50,120)
-rectangle(15,20,'grey')
-
-#head
-t.goto(-85,170)
-rectangle(80,50,'red')
-
-#eyes
-t.goto(-60, 160)
-rectangle(30,10,'white')
-t.goto(-55,155)
-rectangle(5,5,'black')
-t.goto(-40,155)
-rectangle(5,5,'black')
-
-#mouth
-t.goto(-65,135)
-rectangle(40,5,'black')
-
-t.hideturtle()
