fa22880cc86c03b602e6251247484f691e41e2b5
neroghcedrick <134517782+NEROGH2802@users.noreply.github.com>
2023-05-26 18:52:15 +0530

    Add files via upload

--- LEVEL.3  PROGRAME 1.py
+++ LEVEL.3  PROGRAME 1.py
@@ -1,1 +1,7 @@
-print('Hello,word!')
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python311/'hello,world!') 1.py
+Hello,World!
+what is your name g.nerogh
+Hello  g.nerogh
