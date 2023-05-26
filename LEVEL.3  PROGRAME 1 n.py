61295929e7e078110cce5062aa7306231fb92e79
neroghcedrick <134517782+NEROGH2802@users.noreply.github.com>
2023-05-26 19:01:49 +0530

    Add files via upload

---
+++ LEVEL.3  PROGRAME 1 n.py
@@ -0,0 +1,8 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: G:/ylteexperiment 3/arnce/LEVEL.3  PROGRAME 1 no.py =========
+Hello,World!
+What is your name? G.NERODH CEDRICK
+Hello  G.NERODH CEDRICK
+>>> 
