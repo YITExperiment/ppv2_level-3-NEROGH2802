c5adef727f7a23bdc4f4d47f8123e13978be4e5e
neroghcedrick <134517782+NEROGH2802@users.noreply.github.com>
2023-05-26 21:36:16 +0530

    Add files via upload

--- LEVEL.3  PROGRAME 3.py
+++ LEVEL.3  PROGRAME 3.py
@@ -5,17 +5,15 @@ colors = cycle(['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
 
 
 def draw_circle(size,angle,shift):
-    turtle.bgcolor(next(colors))
     turtle.pencolor(next(colors))
-    turtle.circle(size)
-    turtle.right(angle)
-    turtle.forward(shift)
-    draw_circle(size+5, angle+1,shift+1)
+    turtle.circle(size+10)
+    turtle.right(angle+10)
+    turtle.forward(shift+1)
+    draw_circle(size+10, angle+10,shift+1)
     
 turtle.bgcolor('black')
 turtle.speed('fast')
-turtle.pensize(40)
+turtle.pensize(4)
 draw_circle(30,0,1)
-
 
 
