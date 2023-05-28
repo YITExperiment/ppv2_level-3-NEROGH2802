5f4fd62dd9b0760f853d94327f6a394d8b6c0731
neroghcedrick <134517782+NEROGH2802@users.noreply.github.com>
2023-05-28 10:58:44 +0530

    Add files via upload

--- no 1.py
+++ no 1.py
@@ -9,11 +9,11 @@ def draw_circle(size,angle,shift):
     turtle.circle(size)
     turtle.right(angle)
     turtle.forward(shift)
-    draw_circle(size+5, angle+1,shift+1)
+    draw_circle(size+10, angle+10,shift+1)
     
 turtle.bgcolor('black')
 turtle.speed('fast')
 turtle.pensize(4)
-draw_circle(30,0,1)
+draw_circle(10,10,1)
 
 
