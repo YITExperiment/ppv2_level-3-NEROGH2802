3df9f56086f1939903fc866304cf4a0b575143bd
neroghcedrick <134517782+NEROGH2802@users.noreply.github.com>
2023-06-01 12:44:41 +0530

    Add files via upload

---
+++ 3B.py
@@ -0,0 +1,25 @@
+from tkinter import HIDDEN, NORMAL, Tk, Canvas
+root=Tk()
+
+c=Canvas(root, width=400, height=400)
+c.configure(bg='dark blue', highlightthickness=0)
+
+c.body_color = 'SkyBlue1'
+body = c.create_oval(35,20,365,350,outline=c.body_color, fill=c.body_color)
+
+ear_left = c.create_polygon(75,80,75,10,165,70,outline=c.body_color, fill=c.body_color)
+ear_right = c.create_polygon(255,45,325,10,320,70,outline=c.body_color, fill=c.body_color)
+
+foot_right = c.create_oval(65,320,145,360,outline=c.body_color, fill=c.body_color)
+foot_left = c.create_oval(250,320,330,360,outline=c.body_color, fill=c.body_color)
+
+eye_left = c.create_oval(130,110,160,170,outline='black', fill='white')
+pupil_left = c.create_oval(140,145,150,155,outline='black', fill='black')
+
+eye_right = c.create_oval(230,110,260,170,outline='black', fill='white')
+pupil_left = c.create_oval(240,145,250,155,outline='black', fill='black')
+
+mouth_normal = c.create_line(170,250,200,272,230,250, smooth=1, width=2, state=NORMAL)
+
+c.pack()
+root.mainloop()
